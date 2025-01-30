3d390717f160e702dab7a7c525a5b4561d8da235
nico-cbr <ccarvalhonicoly@gmail.com>
2025-01-30 17:28:32 -0300

    explorando botoes e barra inferior

--- um_stateless/lib/main.dart
+++ um_stateless/lib/main.dart
@@ -9,6 +9,8 @@ class Aplicativo extends StatelessWidget{
   
   // apenas para dar identidade ao aplicativo
   const Aplicativo({super.key});
+  
+  get widget => null;
 
   // @override vai dizer que vai sobrescrever a tela
   //build é widget que vai construir toda tela
@@ -17,6 +19,7 @@ class Aplicativo extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           leading: Icon(Icons.ac_unit_rounded, color:  Colors.blueGrey[800]),
@@ -25,19 +28,53 @@ class Aplicativo extends StatelessWidget{
         ),
         body: Center(
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           // Children serve para colocar varios widgets um atras do outro
             children: [ 
               Container(
-              color: const Color.fromARGB(255, 30, 36, 39),
+             
               height: 200,
               width: 200,
               padding: EdgeInsets.only(top: 75),
+              decoration: BoxDecoration(
+                 color: const Color.fromARGB(255, 30, 36, 39),
+                 borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
               child: Text("Olá mundo!",  style: TextStyle(color: Colors.amber[50], fontSize: 30), textAlign: TextAlign.center,),
-              )
+              ),
+
+              Row(
+                // tipo de espaçamento
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                
+                // widget com filhos
+                children: [
+                  Icon(Icons.leaderboard, color: Colors.amber[50], size: 50),
+                  Icon(Icons.person, color: Colors.amber[50], size: 50),
+
+                ],
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+              FloatingActionButton(
+                backgroundColor: Colors.amber[50],
+                child: Icon(Icons.abc, color:  Colors.blueGrey[800], size: 40,),
+                onPressed:() {
+                  print("Parabens, funcionou!");
+                },
+              ),
+              ],
+              ),
             ]   
           )
         ),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(icon: Icon(Icons.construction_rounded), label:"Config"),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label:"Escola"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label:"Buscar"),
+        ]),
       ),
     );
   }
